e194f7dbd27843e1414fa585bd8f0ad607013480
Lyhour <kouchlyhour@gmail.com>
2026-02-23 10:38:56 +0700

    feat: Add `auto.permission` middleware to media dashboard routes.

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Media\Http\Controllers\MediaDashboardController;
 
-Route::middleware(['auth', 'verified'])->prefix('dashboard/media')->name('media.')->group(function () {
+Route::middleware(['auth', 'verified', 'auto.permission'])->prefix('dashboard/media')->name('media.')->group(function () {
     Route::get('/', [MediaDashboardController::class, 'index'])->name('index');
     Route::post('/upload', [MediaDashboardController::class, 'upload'])->name('upload');
     Route::delete('/{id}', [MediaDashboardController::class, 'destroy'])->name('destroy');
